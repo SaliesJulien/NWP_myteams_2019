4acaf8ff786069f2a80cae904420baf37d8d3b61
Julien Salies <J.Salies@pc85.home>
2020-05-05 05:09:37 -0400

    [MODIFIED] user_messages.c // messages quote correction

--- server/src/user_messages.c
+++ server/src/user_messages.c
@@ -7,18 +7,6 @@
 
 #include "server.h"
 
-char *get_command_id(server_t *server)
-{
-    char *id = malloc(sizeof(char) * 37);
-    int k = 0;
-    int i = 0;
-
-    for (i = 0; server->command[i] && server->command[i] != ' '; i++);
-    for (int j = i + 1; server->command[j] && server->command[j] != '\r'; j++)
-        id[k++] = server->command[j];
-    return (id);
-}
-
 void print(int client, int id, server_t *server, int i)
 {
     for (int k = 0; server->clients[id].conversation[i].message[k]; k++)
@@ -40,8 +28,9 @@ void print_messages(server_t *server, int id, char *cmd_id, int client)
 void client_mess(server_t *server, int client, int id)
 {
     bool id_exist = false;
-    char *cmd_id = get_command_id(server);
+    char *cmd_id = parse_first_args(server);
 
+    dprintf(client, "%s\n", cmd_id);
     for (int i = 0; i < server->nb_clients; i++) {
         if (strcmp(cmd_id, server->clients[i].user_id) == 0)
             id_exist = true;
