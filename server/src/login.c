a661dd4ea7a95668459c8e252a9ee828ddebf749
lucas simao <lucas.simao@epitech.eu>
2020-05-05 13:09:13 +0200

    [MODIF] users.c

--- server/src/login.c
+++ server/src/login.c
@@ -7,24 +7,12 @@
 
 #include "server.h"
 
-char *get_sec_arg(char *str)
-{
-    char *tmp = strdup(str);
-    char *arg = NULL;
-
-    if ((arg = strchr(tmp, ' ')) != NULL) {
-        arg[strlen(arg) - strlen(rindex(tmp, '\r'))] = 0;
-        return (arg + 1);
-    }
-    return (NULL);
-}
-
 void login_user(server_t *server, int client, int id)
 {
-    if ((server->clients[id].user_name = get_sec_arg(server->command)) != NULL) {
+    if ((server->clients[id].user_name = parse_first_args(server)) != NULL) {
         server->clients[id].logged = true;
         dprintf(client, "Client logged in.\r\n");
     } else {
-        dprintf(client, "No arguments\r\n");
+        dprintf(client, "Bad arguments\r\n");
     }
 }
