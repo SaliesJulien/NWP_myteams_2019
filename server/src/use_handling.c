987490c418df24f17a5811a263650e36cd3c8a13
Julien Salies <J.Salies@localhost.localdomain>
2020-05-08 09:48:57 -0400

    [MODIFIED] use_handling.c // conding style : OK

--- server/src/use_handling.c
+++ server/src/use_handling.c
@@ -11,7 +11,8 @@ bool use_team(server_t *server, int client, int id, char *team)
 {
     for (int i = 0; server->clients[id].teams[i].team_id != NULL; i++) {
         if (!strcmp(server->clients[id].teams[i].team_id, team)) {
-            server->clients[id].use_state[0] = malloc(sizeof(char) * strlen(team));
+            server->clients[id].use_state[0] =
+                malloc(sizeof(char) * strlen(team));
             server->clients[id].use_state[0] = team;
             dprintf(client, "you entered in team -> \"%s\"\n",
                     server->clients[id].teams[i].team_name);
@@ -27,9 +28,12 @@ bool use_channel(server_t *server, int client, int id, char *channel)
 
     for (i = 0; strcmp(server->clients[id].teams[i].team_id,
         server->clients[id].use_state[0]); i++);
-    for (int k = 0; server->clients[id].teams[i].channel[k].channel_id != NULL; k++)
-        if (!strcmp(server->clients[id].teams[i].channel[k].channel_id, channel)) {
-            server->clients[id].use_state[1] = malloc(sizeof(char) * strlen(channel));
+    for (int k = 0; server->clients[id].teams[i].channel[k].channel_id
+        != NULL; k++)
+        if (!strcmp(server->clients[id].teams[i].channel[k].channel_id,
+                channel)) {
+            server->clients[id].use_state[1] =
+                malloc(sizeof(char) * strlen(channel));
             strcpy(server->clients[id].use_state[1], channel);
             dprintf(client, "you entered in channel -> \"%s\"\n",
                     server->clients[id].teams[i].channel[k].channel_name);
@@ -49,12 +53,14 @@ bool use_thread(server_t *server, int client, int id, char *thread)
     for (k = 0; strcmp(server->clients[id].teams[i].channel[k].channel_id,
         server->clients[id].use_state[1]); k++);
     for (; server->clients[id].teams[i].channel[k].thread[j].thread_id; j++)
-        if (!strcmp(server->clients[id].teams[i].channel[k].thread[j].thread_id, thread)) {
+        if (!strcmp(
+                server->clients[id].teams[i].channel[k].thread[j].thread_id,
+                thread)) {
             server->clients[id].use_state[2] =
                 malloc(sizeof(char) * strlen(thread));
             strcpy(server->clients[id].use_state[2], thread);
             dprintf(client, "you entered in thread -> \"%s\"\n",
-                server->clients[id].teams[i].channel[k].thread[j].thread_title);
+            server->clients[id].teams[i].channel[k].thread[j].thread_title);
             return (true);
         }
     return (false);
