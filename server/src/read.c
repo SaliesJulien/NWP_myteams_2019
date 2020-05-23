cda62051fd5795000464997d941970f56cf9fa57
Hugo <hugo.caulfield@epitech.eu>
2020-05-23 11:41:20 +0200

    [MODIF] read.c / Fix memory leak

--- server/src/read.c
+++ server/src/read.c
@@ -11,7 +11,7 @@ server_t *read_teams(server_t *server, FILE *channel_teams, FILE *thread_teams,
     int i)
 {
     if (channel_teams != NULL) {
-        server->teams[i].channel = malloc((server->teams[i].nb_channel) *
+        server->teams[i].channel = malloc((server->teams[i].nb_channel + 1) *
             sizeof(channel_t));
         fread(server->teams[i].channel, sizeof(channel_t),
             server->teams[i].nb_channel, channel_teams);
@@ -20,9 +20,10 @@ server_t *read_teams(server_t *server, FILE *channel_teams, FILE *thread_teams,
     if (thread_teams != NULL) {
         for (int a = 0; a < server->teams[i].nb_channel; a++) {
             server->teams[i].channel[a].thread = malloc(
-                (server->teams[i].channel[a].nb_thread) * sizeof(thread_t));
+                (server->teams[i].channel[a].nb_thread + 1) * sizeof(thread_t));
             fread(server->teams[i].channel[a].thread, sizeof(thread_t),
                 server->teams[i].channel[a].nb_thread, thread_teams);
+            strcpy(server->teams[i].channel[a].thread[server->teams[i].channel[a].nb_thread].thread_id, "NULL");
         }
     }
     return (server);
