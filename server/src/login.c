24b6798b1f83a15cd90a7ccc5586da678f73fe7d
Hugo <hugo.caulfield@epitech.eu>
2020-05-27 10:44:19 +0200

    [MODIF] login.c / Delete call server event

--- server/src/login.c
+++ server/src/login.c
@@ -27,7 +27,6 @@ bool check_exist(server_t *server, int client, int id, int i)
             server->clients[id].fd_client = -1;
             server->clients[id].active = false;
             dprintf(client, "230 Succesfull login\r\n");
-            server_event_user_logged_in(server->clients[i].user_id);
             send_notification_login(server, i);
             return (true);
         } else {
