0148e2d6b2597673e47f9451e8d810448185f941
Julien Salies <J.Salies@localhost.localdomain>
2020-05-29 12:25:56 -0400

    [MODIFIED] tools.c // count arg command and parse_args function coding style : OK

--- server/src/tools.c
+++ server/src/tools.c
@@ -23,37 +23,70 @@ int uuid_index(server_t *server, char *uuid_find)
     return (-1);
 }
 
-char *parse_args(server_t *server, int wich_args)
+char *return_args(server_t *server, int i, char *cmd, char *error_cmd)
 {
-    char *cmd = NULL;
     int k = 0;
-    int quotes = 0;
-    char *error_cmd = malloc(sizeof(char) * strlen("Bad cmd") + 1);
 
-    error_cmd = strcpy(error_cmd, "Bad cmd");
-    cmd = malloc(sizeof(char) * strlen(server->command));
-    for (int i = 0; server->command[i]; i++) {
-        for (;server->command[i] && server->command[i] == ' '; i++);
-        if (server->command[i] == '"') {
-            quotes++;
-            if (quotes > wich_args) {
-                i++;
-                for (;server->command[i] && server->command[i] != '"'; i++)
-                    cmd[k++] = server->command[i];
-                if (server->command[i] != '"') {
-                    free(cmd);
-                    return (error_cmd);
-                }
-                else {
-                    cmd[k] = '\0';
-                    free(error_cmd);
-                    return (cmd);
-                }
-            }
+    for (;server->command[i] && server->command[i] != '"'; i++)
+        cmd[k++] = server->command[i];
+    if (server->command[i] != '"') {
+        free(cmd);
+        return (error_cmd);
+    }
+    else {
+        cmd[k] = '\0';
+        free(error_cmd);
+        return (cmd);
+    }
+
+}
+
+char *find_arg(server_t *server, int i, char **cmd_array, int *count)
+{
+    if (server->command[i] == '"') {
+        count[0]++;
+        if (count[0] > count[1]) {
+            return (return_args(server, i + 1, cmd_array[0], cmd_array[1]));
         }
     }
-    free(cmd);
-    return (error_cmd);
+    return (NULL);
+}
+
+char *parse_args(server_t *server, int wich_args)
+{
+    int *count = malloc(sizeof(int) * 2);
+    char **cmd_array = malloc(sizeof(char *) * 2);
+    char *temp = malloc(sizeof(char) * strlen(server->command));
+
+    cmd_array[0] = malloc(sizeof(char) * strlen(server->command));
+    cmd_array[1] = malloc(sizeof(char) * strlen("Bad cmd") + 1);
+    count[0] = 0;
+    count[1] = wich_args;
+    strcpy(cmd_array[1], "Bad cmd");
+    for (int i = 0; server->command[i]; i++) {
+        for (;server->command[i] && server->command[i] == ' '; i++);
+        temp = find_arg(server, i, cmd_array, count);
+        if (temp != NULL) {
+            free(count);
+            free(cmd_array);
+            return (temp);
+        }
+    }
+    free(temp);
+    free(cmd_array[0]);
+    return (cmd_array[1]);
+}
+
+int count_quotes(server_t *server, int i, int count, bool name)
+{
+    if (server->command[i] == ' ') {
+        if (!name)
+            count++;
+        for (;server->command[i] && server->command[i] == ' '; i++);
+    }
+    if (server->command[i] == '"')
+        name = !name;
+    return (count);
 }
 
 bool count_args(server_t *server, int args_nb)
@@ -63,13 +96,7 @@ bool count_args(server_t *server, int args_nb)
     int count = 0;
 
     for (; server->command[i]; i++) {
-        if (server->command[i] == ' ') {
-            if (!name)
-                count++;
-            for (;server->command[i] && server->command[i] == ' '; i++);
-        }
-        if (server->command[i] == '"')
-            name = !name;
+        count = count_quotes(server, i, count, name);
     }
     return ((count == args_nb) ? true : false);
 }
