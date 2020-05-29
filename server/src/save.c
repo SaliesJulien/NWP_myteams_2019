064928e478e3f34d1d4ecdaf39e503b4ec3a308f
Hugo <hugo.caulfield@epitech.eu>
2020-05-29 17:08:12 +0200

    [MODIF] save.c / Fix norme issue

--- server/src/save.c
+++ server/src/save.c
@@ -34,22 +34,31 @@ void save_client(server_t *server)
     }
 }
 
+void save_thread(server_t *server, FILE *thread_teams, int i)
+{
+    for (int a = 0; a < server->teams[i].nb_channel; a++) {
+        if (server->teams[i].channel[a].thread == NULL)
+            return;
+        fwrite(server->teams[i].channel[a].thread, sizeof(thread_t),
+            server->teams[i].channel[a].nb_thread, thread_teams);
+    }
+}
+
+void save_channel(server_t *server, FILE *channel_teams, int i)
+{
+    if (server->teams[i].channel == NULL)
+        return;
+    fwrite(server->teams[i].channel, sizeof(channel_t),
+        server->teams[i].nb_channel, channel_teams);
+}
+
 void save_teams(server_t *server, FILE *channel_teams, FILE *thread_teams)
 {
+    if (thread_teams == NULL || channel_teams == NULL)
+        return;
     for (int i = 0; i < server->nb_teams; i++) {
-        if (channel_teams != NULL) {
-            if (server->teams[i].channel == NULL)
-                continue;
-            fwrite(server->teams[i].channel, sizeof(channel_t),
-                server->teams[i].nb_channel, channel_teams);
-        }
-        if (thread_teams != NULL)
-            for (int a = 0; a < server->teams[i].nb_channel; a++) {
-                if (server->teams[i].channel[a].thread == NULL)
-                    continue;
-                fwrite(server->teams[i].channel[a].thread, sizeof(thread_t),
-                    server->teams[i].channel[a].nb_thread, thread_teams);
-            }
+        save_channel(server, channel_teams, i);
+        save_thread(server, thread_teams, i);
     }
 }
 
